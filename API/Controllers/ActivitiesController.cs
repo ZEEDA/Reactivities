983d7687dff32677e7c0e62f77c140b9f5fd6576
Zeeda Ezz <mohamedezz195@gmail.com>
2026-09-14 22:58:09 +0300

    Initial commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,39 @@
+using System;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+public class ActivitiesController(AppDbContext context) : BaseApiController
+{
+    /* -------
+     * The Old way
+    */
+
+    // private readonly AppDbContext context;
+
+    // public ActivitiesController(AppDbContext context)
+    // {
+    //     this.context = context;
+    // }
+    
+    /* -------
+     * The New way is to use a primary constructor to inject dependencies.
+    */
+
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetails(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+        if (activity == null) return NotFound();
+        return activity;
+    }
+}
